0d9e751b588b96890eb8db7a861a999c0941a3dd
cycjimmy <cycjimmy@gmail.com>
2019-08-08 16:15:07 +0800

    IntroductionToClasses

---
+++ IntroductionToClasses/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace IntroductionToClasses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 教程：https://docs.microsoft.com/zh-cn/dotnet/csharp/tutorials/intro-to-csharp/introduction-to-classes
+
+            var account = new BankAccount("Geoffrey", 1000);
+            Console.WriteLine(
+                $"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+
+            account.MakeWithdrawal(500, DateTime.Now, "Rent payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back");
+            Console.WriteLine(account.Balance);
+
+            // Test that the initial balances must be positive
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance");
+                Console.WriteLine(e.ToString());
+            }
+
+            // Test for a negative balance;
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
